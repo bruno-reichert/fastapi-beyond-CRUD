12997c80575ffcdbd0af02efd3d3dd6b404ea4ed
Bismarck <brunowasborn2rock@gmail.com>
2026-06-17 15:39:29 -0300

    oops

--- bookly/src/__init__.py
+++ bookly/src/__init__.py
@@ -6,7 +6,7 @@ from src.reviews.routes import reviews_router
 from contextlib import asynccontextmanager
 from src.db.main import init_db
 from .errors import register_all_errors
-from .middleware import register_middleware
+# from .middleware import register_middleware
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
@@ -29,7 +29,7 @@ app = FastAPI(
     })
 
 register_all_errors(app)
-register_middleware(app)
+# register_middleware(app)
 
 app.include_router(book_router, prefix=f"/api/{version}/books", tags=["books"])
 app.include_router(auth_router, prefix=f"/api/{version}/auth", tags=["auth"])
